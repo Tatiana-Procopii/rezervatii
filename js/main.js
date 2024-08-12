67b3d2ef47f31707c3f19a7c3349177301fc0abf
Tatiana-Procopii <80848421+Tatiana-Procopii@users.noreply.github.com>
2024-08-12 13:21:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,3 +6,27 @@ function toggleMenu() {
         menu.style.display = 'block';
     }
 }
+
+emailjs.init("c1pdO4HeYDPxv-pzT");
+function send() {
+
+    if (document.getElementById("formular").checkValidity() == false) {
+        document.getElementById("formular").reportValidity();
+        return;
+    }
+
+    var date = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        mesaj: document.getElementById("mesaj").value
+    };
+
+    emailjs.send("service_wdq5j4l", "template_a3c043a", date)
+        .then(function (raspuns) {
+            alert("Mesajul a fost transmis.")
+            document.getElementById("formular").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+            console.log(error);
+        })
+}
